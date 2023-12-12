aadb6b77456d982aa91fa297a66ef4852c2201cb
Manojreddyayuluri <153638637+Manojreddyayuluri@users.noreply.github.com>
2023-12-12 23:34:27 +0530

    Add files via upload

---
+++ m2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>  
+int main() {  
+    int triangle[5][5];   
+    for (int i = 0; i< 5; i++) {  
+        triangle[0][i] = 1;  
+    }  for (int i = 1; i< 5; i++) {  
+        for (int j = 0; j <= i; j++) {  
+            if (j == 0 || j == i) {  
+                triangle[i][j] = 1;  
+            } else {  
+                triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j];  
+            }  
+        }  
+    }  for (int i = 0; i< 5; i++) {  
+        for (int j = 0; j <= i; j++) {  
+printf("%d ", triangle[i][j]);  
+        }  
+printf("\n");  
+    }  
+  
+    return 0;  
+}  
